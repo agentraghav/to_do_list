b005b8cc66030fc6b8b739d67c826bdbaaf95588
agentraghav <raghvendrarathore2@gmail.com>
2021-01-16 13:20:20 +0530

    body

--- src/components/body.js
+++ src/components/body.js
@@ -4,9 +4,14 @@ import '../styles/styles.css';
 export const Body = () => {
   return (
     <Container fluid className='back'>
-      <Row>
-        <Col className='box'>
-          <h1>Hello World</h1>
+      <Row className='box'>
+        <Col>
+          <h2 className='head'>
+            To Do List <i class='fas fa-pencil-alt'></i>
+          </h2>
+        </Col>
+        <Col>
+          <input type='text' id='list' onClick={handleInput} />
         </Col>
       </Row>
     </Container>

7798bc60bf4d9dea5ea57479afe212647b52a977
agentraghav <raghvendrarathore2@gmail.com>
2021-01-16 14:50:16 +0530

    to do

--- src/components/body.js
+++ src/components/body.js
@@ -1,7 +1,27 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Row, Col, Alert } from 'react-bootstrap';
 import '../styles/styles.css';
 export const Body = () => {
+  let [list, setList] = useState(
+    JSON.parse(window.localStorage.getItem('list')) || []
+  );
+  let curr_value;
+  const handleInput = (e) => {
+    curr_value = e.target.value;
+  };
+
+  const handleAdd = (e) => {
+    if (curr_value !== '') {
+      const obj = { id: '', todo: curr_value };
+      list = list.map((todo, index) => {
+        return { ...todo, id: index };
+      });
+      list.push({ ...obj, id: list.length });
+      window.localStorage.setItem('list', JSON.stringify(list));
+      window.location.reload();
+    }
+  };
+
   return (
     <Container fluid className='back'>
       <Row className='box'>
@@ -9,9 +29,26 @@ export const Body = () => {
           <h2 className='head'>
             To Do List <i class='fas fa-pencil-alt'></i>
           </h2>
-        </Col>
-        <Col>
-          <input type='text' id='list' onClick={handleInput} />
+          <Col className='inp'>
+            <input
+              onChange={handleInput}
+              required
+              type='text'
+              id='list'
+              placeholder='Add to your list'
+            />
+            <i
+              className='fa fa-plus add'
+              onClick={handleAdd}
+              aria-hidden='true'></i>
+          </Col>
+          <Col>
+            {list.map((item, index) => (
+              <Alert variant='danger' className='li-item' key={index}>
+                <i class='fa fa-times del' aria-hidden='true'></i>'{item.todo}
+              </Alert>
+            ))}
+          </Col>
         </Col>
       </Row>
     </Container>

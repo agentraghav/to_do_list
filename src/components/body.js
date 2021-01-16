99df775c5d488efa6a86fe8f28e1abefa18442fa
agentraghav <raghvendrarathore2@gmail.com>
2021-01-16 12:31:05 +0530

    body

---
+++ src/components/body.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import '../styles/styles.css';
+export const Body = () => {
+  return (
+    <Container fluid>
+      <Row className='back'></Row>
+    </Container>
+  );
+};
